f0c4381e6808122b67d5a36cb8d0e82eebc61030
Alessandro Gautieri <113530268+Alegau03@users.noreply.github.com>
2025-03-15 08:48:21 +0100

    ailab

--- Terzo Anno/Secondo Semestre/AiLab/documentScanner2.py
+++ Terzo Anno/Secondo Semestre/AiLab/documentScanner2.py
@@ -8,11 +8,10 @@ def onClick(event,x,y,flags,params):
     if event == cv2.EVENT_LBUTTONDOWN:
         #controllo se sono già a 4 punti
         if len(scr_points)<4:
-            scr_points.append([x,y])
+            scr_points.append((x,y))
             #disegno un cerchio nel punto
-            cv2.circle(img_copy,(x,y),10(0,255,0),-1)
+            cv2.circle(img_copy,(x,y),15,(0,255,0),-1)
             cv2.imshow("Img",img_copy)
-            cv2.waitKey(0)
 
 img=cv2.imread("Data/01-Data/gerry.png")
 
@@ -21,22 +20,23 @@ img_copy = img.copy()
 
 
 #creo il source point, inizialmente vuoto perchè lo riempirò con i punti che clicco
-scr_points=np.float32([])
+scr_points=[]
 dst_points= np.float32([
     [0,0],
-    [0,800],
-    [600,800],
-    [600,0]
+    [0,600],
+    [800,600],
+    [800,0]
 ])
 
 #creo la finestra che prenderà i click
 cv2.namedWindow("Img",cv2.WINDOW_KEEPRATIO)
-cv2.setMouseCallback("Img", onClick)
 cv2.imshow("Img",img_copy)
+cv2.setMouseCallback("Img", onClick)
 cv2.waitKey(0)
 
 #get the homography matrix
-H = cv2.getPerspectiveTransform(scr_points,dst_points)
+scr2= np.float32(scr_points)
+H = cv2.getPerspectiveTransform(scr2,dst_points)
 #apply the homography matrix
 new_img=cv2.warpPerspective(img,H,(600,800))
 cv2.namedWindow("Result",cv2.WINDOW_KEEPRATIO)
--- Terzo Anno/Secondo Semestre/AiLab/scanner3.py
+++ Terzo Anno/Secondo Semestre/AiLab/scanner3.py
@@ -1,4 +1,44 @@
-import gradio
+import gradio as gr
 import numpy as np
 import cv2
 
+src_points=[]
+def fixImg(img):
+    dst_points= np.float32([
+        [0,0],
+        [0,600],
+        [800,600],
+        [800,0]
+            ])
+    scr_float=np.float32(src_points)
+    H = cv2.getPerspectiveTransform(scr_float,dst_points)
+    new_img=cv2.warpPerspective(img,H,(600,800))
+    return new_img
+def onSelect(value,evt: gr.EventData):
+    if len(src_points)<4:
+        src_points.append(evt._data['index'])
+    return len(src_points)
+    
+def cancella(img):
+    src_points.clear()
+    inp.clear()
+    out.clear()
+    return 
+with gr.Blocks() as demo:
+    gr.Markdown("# Document Scanner")
+    gr.Markdown("Click on the four corners of the document to scan it in counter clockways")
+    coord_n= gr.Textbox(label="Coordinates")
+    with gr.Row():
+        inp= gr.Image(label="Input")
+        #evento per i click sugli angoli
+        inp.select(fn = onSelect,inputs=inp,outputs=coord_n)
+        
+        out= gr.Image(label="Output")
+    with gr.Row():
+        btn= gr.Button("Fix!",variant='primary',size='lg')
+        btn.click(fn=fixImg, inputs=inp, outputs=out)      
+        
+    with gr.Row():
+        btnCancella= gr.Button("Cancel",variant='danger',size='lg')
+        btnCancella.click(fn=cancella, inputs=inp, outputs=out)
+demo.launch()
